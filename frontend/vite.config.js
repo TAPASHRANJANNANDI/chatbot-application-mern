e2b39f182515dd4404ff39671e5ee69cdf708f06
Tapash Ranjan Nandi <tapashnandi777@gmail.com>
2025-11-20 12:33:07 +0530

    commited vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,
+    allowedHosts: [
+      "http://a41816939f96e4a9080fcc9b15ce97ea-548470199.ap-south-1.elb.amazonaws.com",
+      "http://a41816939f96e4a9080fcc9b15ce97ea-548470199.ap-south-1.elb.amazonaws.com:5173"
+    ]
+  }
 })
