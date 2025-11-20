e427ab1867657ef4cd86784ee058397d41818a83
Tapash Ranjan Nandi <tapashnandi777@gmail.com>
2025-11-20 16:22:24 +0530

    commited from main

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,7 @@ mongoose.connect(process.env.MONGO_URI)
 }).catch((error)=>{
     console.log("Error connecting to MongoDB:", error)
 })
-
+//Testing changes
 // Defining Routes
 app.use("/bot/v1/", chatbotRoutes)
 
